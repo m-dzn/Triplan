ebe8958c011ecd550aa2f47964bf9d1f7ea3f1f0
Heera <92657209+mykuromi@users.noreply.github.com>
2021-10-31 00:37:50 +0900

    10.31(sun) CRUD

--- customer/src/main/java/com/triplan/controller/RoomController.java
+++ customer/src/main/java/com/triplan/controller/RoomController.java
@@ -3,10 +3,7 @@ package com.triplan.controller;
 import com.triplan.domain.RoomVO;
 import com.triplan.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +16,24 @@ public class RoomController {
     public String roomInsert(@RequestBody RoomVO roomVO) {
         roomService.roomInsert(roomVO);
         return "room insert succeed";
+    }
+
+    @GetMapping("/{room_id}")
+    public RoomVO roomSelect(@PathVariable int room_id) {
+        return roomService.roomSelect(room_id);
+    }
+
+    @PutMapping("/{room_id}")
+    public String roomUpdate(@PathVariable int room_id, @RequestBody RoomVO roomVO) {
+        roomVO.setRoom_id(room_id);
+        roomService.roomUpdate(roomVO);
+        return "room update succeed";
+    }
+
+    @DeleteMapping("/{room_id}")
+    public String roomDelete(@PathVariable int room_id) {
+        roomService.roomDelete(room_id);
+        return "room delete succeed";
     }
 
 }
--- customer/src/main/java/com/triplan/service/RoomServiceImpl.java
+++ customer/src/main/java/com/triplan/service/RoomServiceImpl.java
@@ -15,4 +15,19 @@ public class RoomServiceImpl implements RoomService{
     public void roomInsert(RoomVO roomVO) {
         roomMapper.roomInsert(roomVO);
     }
+
+    @Override
+    public RoomVO roomSelect(int room_id) {
+        return roomMapper.roomSelect(room_id);
+    }
+
+    @Override
+    public void roomUpdate(RoomVO roomVO) {
+        roomMapper.roomUpdate(roomVO);
+    }
+
+    @Override
+    public void roomDelete(int room_id) {
+        roomMapper.roomDelete(room_id);
+    }
 }
