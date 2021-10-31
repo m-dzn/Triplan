0d1b7d2ef46cc0175718ad6db4b95171c10b6446
Son Myungjin <shonmyungjin@naver.com>
2021-10-31 16:40:29 +0900

    Seller CRUD 및 ENUM 추가

--- core/src/main/java/com/triplan/domain/SellerVO.java
+++ core/src/main/java/com/triplan/domain/SellerVO.java
@@ -1,4 +1,26 @@
 package com.triplan.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
 public class SellerVO {
+    private Long sellerId;
+    private Long memberId;
+    private String productGroup;
+    private String businessLicenseNumber;
+    private String businessName;
+    private String tel;
+    private String ownerName;
+    private String address;
+    private String zipCode;
+    private String email;
+    private String sellerImg;
+    private String businessAccount;
+    private String personalOrCompany;
 }
