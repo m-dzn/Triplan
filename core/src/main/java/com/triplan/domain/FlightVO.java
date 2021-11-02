c0f3d27e98c9ae86916c3119c2f6b50c081f2a1f
songnain <thdskdls@gmail.com>
2021-11-02 12:00:16 +0900

    add enum class

--- core/src/main/java/com/triplan/domain/FlightVO.java
+++ core/src/main/java/com/triplan/domain/FlightVO.java
@@ -1,4 +1,40 @@
 package com.triplan.domain;
 
+import com.triplan.enumclass.FlightType;
+import com.triplan.enumclass.PassengerType;
+import com.triplan.enumclass.SeatClass;
+import lombok.Data;
+
+
+@Data
 public class FlightVO {
+
+    private Integer flightId;
+    private Integer itemId;
+    private String departure;
+    private String destination;
+    private FlightType flightType;
+    private Integer numberOfPerson;
+    private SeatClass seatClass;
+    private PassengerType passengerType;
+    private String airline;
+    private String baggage;
+    private String seatNumber;
+
+    public static void main(String[] args) {
+        FlightVO flightVO = new FlightVO();
+        flightVO.setFlightType(FlightType.ONE_WAY);
+        flightVO.setPassengerType(PassengerType.ADULT);
+        flightVO.setSeatClass(SeatClass.ECONOMY);
+
+        System.out.println(flightVO.getFlightType());
+        System.out.println(flightVO.getFlightType().getExplain());
+        System.out.println();
+        System.out.println(flightVO.getPassengerType());
+        System.out.println(flightVO.getPassengerType().getPerson());
+        System.out.println();
+        System.out.println(flightVO.getSeatClass());
+        System.out.println(flightVO.getSeatClass().getSit());
+
+    }
 }
