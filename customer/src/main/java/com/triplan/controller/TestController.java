3f33f8baaa25e779e83bebb836683de3edfdd2bd
choigy <leeniesta@naver.com>
2021-11-02 23:24:31 +0900

    프론트_이민재 : 로그인/회원가입 임시구현 + 메인 페이지/마이 페이지-프로필 관리 진행 중 + 글꼴 및 UI 꾸미는 중

---
+++ customer/src/main/java/com/triplan/controller/TestController.java
@@ -0,0 +1,40 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.triplan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    public TestController() {
+    }
+
+    @GetMapping({"/warning"})
+    public String warning() {
+        return "warning";
+    }
+
+    @GetMapping({"/main"})
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping({"/mypage"})
+    public String mypage() {
+        return "mypage";
+    }
+
+    @GetMapping({"/login"})
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping({"/join"})
+    public String join() {
+        return "join";
+    }
+}
