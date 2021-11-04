29e30afc4e01b8702fc137bf69803318e3be0d07
jamongjelly <88yangkh@gmail.com>
2021-11-04 12:15:28 +0900

    feat: SellerVO 추가

--- core/src/main/java/com/triplan/domain/SellerVO.java
+++ core/src/main/java/com/triplan/domain/SellerVO.java
@@ -1,4 +1,24 @@
 package com.triplan.domain;
 
+import com.triplan.enumclass.PersonalOrCompany;
+import com.triplan.enumclass.ProductGroup;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SellerVO {
+    private Integer sellerId;
+    private Integer memberId;
+    private ProductGroup productGroup;
+    private String businessLicenseNumber;
+    private String businessName;
+    private String tel;
+    private String ownerName;
+    private String address;
+    private String zipCode;
+    private String email;
+    private String sellerImg;
+    private String businessAccount;
+    private PersonalOrCompany personalOrCompany;
 }
